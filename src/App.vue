01543f796fa109e5a2b4d59f815a7b0b56e7ab64
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-06-02 13:24:11 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+
+import axios from 'axios';
+
+export default {
+  methods: {
+    data() {
+      return {
+        'projects': []
+      }
+    },
+    getApi(){
+      axios.get("http://127.0.0.1:8000/api/projects")
+      .then ( result => this.projects = result.data
+      )
+    },
+  },
+  mounted(){
+   this.getApi()
+  }
+}
+</script>
+
+<template>
+  <div class="main-wrapper">
+   
+   <ul>
+    <li v-for="project in projects" 
+    :key="project.id">
+      {{$project.title}} 
+      <!--{{$project.author}} 
+      {{$project.date_of_creation}} 
+      {{$project.description}} -->
+    </li>
+   </ul>
+
+  </div>
+</template>
+
+<style>
+
+</style>
