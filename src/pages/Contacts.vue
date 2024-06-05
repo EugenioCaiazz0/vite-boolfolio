627f60f12209ea3425b6489a2dd78e0c139f4cf8
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-06-05 15:52:54 +0200

    Passaggio dati frontend-backend

---
+++ src/pages/Contacts.vue
@@ -0,0 +1,62 @@
+<script>
+import axios from 'axios';
+import { store } from '../data/store';
+
+
+export default {
+    name: "Contacts",
+    data() {
+        return {
+            name : '',
+            email : '',
+            message : ''
+        }
+    },
+    methods: {
+        sendEmail() {
+            const data = {
+                name: this.name,
+                email: this.email,
+                message: this.message
+            }
+
+            axios.post(store.apiUrl + '/sendEmail', data)
+                .then(result => {
+                    console.log(result.data);
+                }) 
+
+            console.log(data);
+        }
+    },
+}
+
+</script>
+
+<template>
+   
+   <div>
+        <form @submit.prevent="sendEmail">
+            <div>
+                <div>
+                    <label for="name"> Nome </label>
+                    <input v-model="name" type="text" name="names" id="name"/>
+                </div>
+                <div>
+                    <label for="email"> E-mail </label>
+                    <input v-model="email" type="text" name="email" id="email"/>
+                </div>
+                <div>
+                    <label for="message"> Messaggio </label>
+                    <textarea v-model="message" name="message" id="message"> </textarea> 
+                </div>
+                <div>
+                    <button type="submit"> Invia </button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+</template>
+
+<style>
+</style>
--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import Home from './pages/Home.vue';
 import About from './pages/About.vue';
 import Projects from './pages/Projects.vue';
 import Error404 from './pages/Error404.vue';
-
+import Contacts from './pages/Contacts.vue';
 
 const router = createRouter({
     history : createWebHistory(),
@@ -23,6 +23,11 @@ const router = createRouter({
         path: '/projects',
         name: 'projects',
         component: Projects
+    },
+    {
+        path: '/contacts',
+        name: 'contacts',
+        component: Contacts
     },
     {
         path: '/:pathMatch(.*)*',
